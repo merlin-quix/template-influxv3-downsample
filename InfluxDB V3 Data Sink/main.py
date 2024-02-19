5e049928414fefe0866a82265eac6360a2cc170e
Merlin Carter <merlin+demo@quix.io>
2024-02-19 15:49:22 +0000

    Edited 'main.py'

--- InfluxDB V3 Data Sink/main.py
+++ InfluxDB V3 Data Sink/main.py
@@ -11,7 +11,7 @@ app = Application.Quix(consumer_group="influx-destinationv4",
 input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
 
 # Read the environment variable and convert it to a list
-tag_columns = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "[]"))
+tag_columns = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "{}"))
 
 # Read the environment variable for measurement name and convert it to a list
 measurement_name = os.environ.get('INFLUXDB_MEASUREMENT_NAME', os.environ["input"])
